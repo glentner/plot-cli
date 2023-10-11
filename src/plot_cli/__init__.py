47aff40c66f756fbe724c0798e5adab3c7afed1b
Geoffrey Lentner <glentner@purdue.edu>
2023-10-11 12:59:54 -0400

    Fixup command interface

--- src/plot_cli/__init__.py
+++ src/plot_cli/__init__.py
@@ -11,7 +11,8 @@ from typing import Tuple, Dict, List, Type, Optional, Any
 # standard libs
 import os
 import sys
-import logging
+import re
+from importlib.metadata import version as get_version
 from itertools import cycle
 from functools import partial, cached_property
 from shutil import get_terminal_size
@@ -20,67 +21,67 @@ from shutil import get_terminal_size
 from cmdkit.app import Application, exit_status
 from cmdkit.cli import Interface
 from cmdkit.config import ConfigurationError
+from cmdkit.logging import Logger
+from cmdkit.ansi import italic, bold, COLOR_STDOUT, colorize_usage as default_colorize_usage
 
 # internal libs
-from plot_cli.core.exceptions import write_traceback
-from plot_cli.core.logging import initialize_logging
+from plot_cli.config import write_traceback
 from plot_cli.provider import PlotInterface, TPlot, TPlotLine, TPlotHist
 from plot_cli.data import DataSet
 
 # public interface
-__all__ = ['main', 'PlotApp', '__version__', ]
-
-# metadata
-__version__ = '0.2.0'
+__all__ = ['main', 'PlotApp', ]
 
 # application logger
-log = logging.getLogger('plot_cli')
+log = Logger.with_name(__name__)
 
 
-APP_NAME = 'plot_cli'
+APP_NAME = os.path.basename(sys.argv[0])
 APP_USAGE = f"""\
-usage: {APP_NAME} [-h] [-v] FILE ...
-Simple command-line plotting tool.\
+Usage: 
+  {APP_NAME} [-h] [-v] [FILE] [-x NAME] [-y NAME] [--line | --hist] ...
+  Simple command-line plotting tool.\
 """
 
 APP_HELP = f"""\
 {APP_USAGE}
 
-arguments:
-FILE                              Path to data file (default: <stdin>).
+Arguments:
+  FILE                        Path to data file (default: <stdin>).
 
-options:
--x, --xdata             NAME      Field to use for x-axis.
--y, --ydata             NAME...   Field(s) for y-axis.
--b, --backend           NAME      Method for plotting data (default: tplot).
-    --plot_cli-type         NAME      Type of plot_cli to create (default: line).
-    --line                        Alias for --plot_cli-type=line
-    --hist                        Alias for --plot_cli-type=hist
+Options:
+  -x, --xdata        NAME     Field to use for x-axis.
+  -y, --ydata        NAME...  Field(s) for y-axis.
+      --backend      NAME     Method for plotting data (default: tplot).
+      --plot-type    NAME     Type of plot_cli to create (default: line).
+      --line                  Alias for --plot-type=line
+      --hist                  Alias for --plot-type=hist
 
-formatting:
-    --xlabel            STR       Content for x-axis label.
-    --ylabel            STR       Content for y-axis label.
--t, --title             STR       Content for plot_cli title.
--c, --color             SEQ       Comma-separated color names (e.g., blue,red,green).
--s, --size              SHAPE     Width and height in pixels (default: 100,20).
+Formatting:
+      --xlabel       NAME     Content for x-axis label.
+      --ylabel       NAME     Content for y-axis label.
+  -t, --title        NAME     Content for plot_cli title.
+  -c, --color        SEQ      Comma-separated color names (e.g., 'blue,red,green').
+  -s, --size         SHAPE    Width and height in pixels (default: 100,20).
+  -l, --legend       POS      Legend position (default: 'bottomright').
 
-histogram:
-    --bins              NUM       Number of bins for histogram (default: 10).
-    --density                     Display frequency as percentage.
+Histogram:
+  -b, --bins         NUM      Number of bins for histogram (default: 10).
+  -d, --density               Display frequency as percentage.
 
-timeseries:
-    --timeseries        [NAME]    Fields to parse as date and/or time.
-    --timeseries-scale  SCALE     Re-scale datetime axis (e.g., +/- hours).
-    --resample          FREQ      Re-sample on some frequency (e.g., '1min').
-    --agg-method        NAME      Aggregation method (required for --resample).
-    --mean                        Alias for --agg-method=mean.
-    --sum                         Alias for --agg-method=sum.
-    --count                       Alias for --agg-method=count.
-    --max                         Alias for --agg-method=max.
-    --min                         Alias for --agg-method=min.
+Timeseries:
+  -T, --timeseries  [NAME]    Fields to parse as date and/or time.
+  -S, --scale        SCALE    Re-scale datetime axis (e.g., +/- hours).
+  -F, --resample     FREQ     Re-sample on some frequency (e.g., '1min').
+  -A, --agg-method   NAME     Aggregation method (required for --resample).
+      --mean                  Alias for --agg-method=mean.
+      --sum                   Alias for --agg-method=sum.
+      --count                 Alias for --agg-method=count.
+      --max                   Alias for --agg-method=max.
+      --min                   Alias for --agg-method=min.
 
--h, --help                        Show this message and exit.
--v, --version                     Show the version and exit.\
+  -h, --help                  Show this message and exit.
+  -v, --version               Show the version and exit.\
 """
 
 
@@ -108,11 +109,41 @@ def split_size(spec: str, sep: str = ',') -> Tuple[float, float]:
     return width, height
 
 
+# Look-around pattern to negate matches within quotation marks
+# Whole quotations are formatted together
+NOT_QUOTED = (
+    r'(?=([^"]*"[^"]*")*[^"]*$)' +
+    r"(?=([^']*'[^']*')*[^']*$)" +
+    r'(?=([^`]*`[^`]*`)*[^`]*$)'
+)
+
+
+def format_special_args(text: str) -> str:
+    """Formatting special arguments."""
+    metavars = ['SEQ', 'SHAPE', 'POS', 'SCALE', 'FREQ']
+    metavars_pattern = r'\b(?P<arg>' + '|'.join(metavars) + r')\b'
+    return re.sub(metavars_pattern + NOT_QUOTED, italic(r'\g<arg>'), text)
+
+
+def format_headers(text: str) -> str:
+    """Formatting section headers."""
+    names = ['Formatting', 'Histogram', 'Timeseries']
+    return re.sub(r'(?P<name>' + '|'.join(names) + r'):' + NOT_QUOTED, bold(r'\g<name>:'), text)
+
+
+def colorize_usage(text: str) -> str:
+    """Apply additional formatting to usage/help text."""
+    if not COLOR_STDOUT:  # NOTE: usage is on stdout not stderr
+        return text
+    else:
+        return default_colorize_usage(format_headers(format_special_args(text)))
+
+
 class PlotApp(Application):
     """Main application class."""
 
-    interface = Interface(APP_NAME, APP_USAGE, APP_HELP)
-    interface.add_argument('-v', '--version', action='version', version=__version__)
+    interface = Interface(APP_NAME, APP_USAGE, APP_HELP, formatter=colorize_usage)
+    interface.add_argument('-v', '--version', action='version', version=get_version(__name__))
 
     source: str = '-'
     interface.add_argument('source', nargs='?', default=source)
@@ -124,7 +155,7 @@ class PlotApp(Application):
 
     plot_type: str = 'line'
     plot_type_interface = interface.add_mutually_exclusive_group()
-    plot_type_interface.add_argument('--plot_cli-type', default=plot_type, choices=['line', ])
+    plot_type_interface.add_argument('--plot-type', default=plot_type, choices=['line', ])
     plot_type_interface.add_argument('--line', action='store_const', const='line', dest='plot_type')
     plot_type_interface.add_argument('--hist', action='store_const', const='hist', dest='plot_type')
 
@@ -134,18 +165,18 @@ class PlotApp(Application):
     ydata: Optional[List[str]] = []
     interface.add_argument('-y', '--ydata', nargs='*', default=[])
 
-    timeseries: str = None
-    interface.add_argument('--timeseries', action='store_const', default=None, const='-')
+    timeseries: bool = False
+    interface.add_argument('-T', '--timeseries', action='store_true')
 
     timeseries_scale: Optional[str] = None
-    interface.add_argument('--timeseries-scale', default=None)
+    interface.add_argument('-S', '--scale', default=None, dest='timeseries_scale')
 
     resample_freq: str = None
-    interface.add_argument('--resample', default=None, dest='resample_freq')
+    interface.add_argument('-F', '--resample', default=None, dest='resample_freq')
 
     agg_method: str = None
     agg_interface = interface.add_mutually_exclusive_group()
-    agg_interface.add_argument('--agg-method', default=None, choices=['mean', 'sum', 'count', 'max', 'min'])
+    agg_interface.add_argument('-A', '--agg-method', default=None, choices=['mean', 'sum', 'count', 'max', 'min'])
     agg_interface.add_argument('--mean', action='store_const', const='mean', dest='agg_method')
     agg_interface.add_argument('--sum', action='store_const', const='sum', dest='agg_method')
     agg_interface.add_argument('--count', action='store_const', const='count', dest='agg_method')
@@ -156,7 +187,7 @@ class PlotApp(Application):
     interface.add_argument('-b', '--bins', type=int, default=hist_bins, dest='hist_bins')
 
     hist_density: bool = False
-    interface.add_argument('--density', action='store_true', dest='hist_density')
+    interface.add_argument('-d', '--density', action='store_true', dest='hist_density')
 
     drop_missing: bool = False
     interface.add_argument('--drop-missing', action='store_true')
@@ -168,16 +199,17 @@ class PlotApp(Application):
     interface.add_argument('-t', '--title', default=None)
 
     xlabel: Optional[str] = None
-    interface.add_argument('--xlabel', default=None)
+    interface.add_argument('-X', '--xlabel', default=None)
 
     ylabel: Optional[str] = None
-    interface.add_argument('--ylabel', default=None)
+    interface.add_argument('-Y', '--ylabel', default=None)
 
     size: Optional[Tuple[float, float]] = None
     interface.add_argument('-s', '--size', default=None, type=split_size)
 
-    legend: str = 'bottomleft'
-    interface.add_argument('--legend', default='bottomleft')
+    legend: str = 'bottomright'
+    interface.add_argument('-l', '--legend', default=legend,
+                           choices=['bottomleft', 'bottomright', 'topleft', 'topright'])
 
     log_critical = log.critical
     log_exception = log.exception
@@ -216,14 +248,18 @@ class PlotApp(Application):
             return self.xlabel
         if self.timeseries and self.timeseries_scale:
             return f'{self.dataset.index.name} ({self.timeseries_scale})'
+        if self.plot_type == 'hist':
+            return ''
         else:
-            return self.dataset.index.name
+            return f'{self.dataset.index.name}'
 
     @cached_property
     def plot_ylabel(self: PlotApp) -> str:
         """Y-axis label."""
         if self.ylabel:
             return self.ylabel
+        if self.plot_type == 'hist' and self.hist_density:
+            return 'percent'
         else:
             return 'value'
 
@@ -258,12 +294,13 @@ class PlotApp(Application):
         else:
             log.info(f'Reading from {self.source}')
             self.dataset = DataSet.from_local(self.source)
+        xcol = self.xdata or self.dataset.columns[0]
         if self.timeseries:
             ts_column = self.timeseries if self.timeseries != '-' else self.dataset.columns[0]
             log.info(f'Parsing timeseries ({ts_column})')
-            self.dataset.set_type(ts_column, 'datetime64[ns]')
+            self.dataset.set_type(xcol, 'datetime64[ns]')
         scale_for_index = self.timeseries_scale if not self.resample_freq else None
-        self.dataset.set_index(self.xdata or self.dataset.columns[0], timeseries_scale=scale_for_index)
+        self.dataset.set_index(xcol, timeseries_scale=scale_for_index)
         if self.resample_freq:
             self.dataset.resample(self.resample_freq, agg=self.agg_method, scale=self.timeseries_scale)
         if self.drop_missing:
@@ -272,5 +309,4 @@ class PlotApp(Application):
 
 def main() -> int:
     """Entry-point for console application."""
-    initialize_logging()
     return PlotApp.main(sys.argv[1:])
