18698736a6547d5df7b0b022621d5a654ae16aa3
Geoffrey Lentner <glentner@purdue.edu>
2023-10-11 12:59:08 -0400

    Drop core modules
    CmdKit provides all boilerplate.
    Moved remaining setup to top-level config module.

--- src/plot_cli/config.py
+++ src/plot_cli/config.py
@@ -1,46 +1,54 @@
 # SPDX-FileCopyrightText: 2022 Geoffrey Lentner
 # SPDX-License-Identifier: Apache-2.0
 
-"""Core exception handling."""
+"""Configuration management."""
 
 
 # type annotations
 from typing import Union
 
-# standard libraries
+# standard libs
 import os
 import sys
-import logging
 import functools
 import traceback
 from datetime import datetime
 
 # external libs
+from cmdkit.config import Configuration
+from cmdkit.namespace import Namespace
+from cmdkit.logging import Logger, level_by_name, logging_styles
+from cmdkit.ansi import magenta, bold, faint
 from cmdkit.app import exit_status
-from cmdkit.config import Namespace
 
-# internal libs
-from plot_cli.core.platform import path as default_path
 
 # public interface
-__all__ = ['display_critical', 'traceback_filepath', 'write_traceback', ]
+__all__ = ['default_config', 'config', 'context', 'write_traceback']
+
+
+default_config = {
+    'logging': {
+        'level': 'warning',
+        **logging_styles.get('default'),
+    },
+}
 
 
 def display_critical(error: Union[Exception, str], module: str = None) -> None:
     """Apply basic formatting to exceptions (i.e., without logging)."""
     text = error if isinstance(error, str) else f'{error.__class__.__name__}: {error}'
-    name = '' if not module else f'[{module}]'
-    print(f'CRITICAL {name} {text}', file=sys.stderr)
+    name = faint(f'[{module or "plot"}]')
+    print(f'{bold(magenta("CRITICAL"))} {name} {text}', file=sys.stderr)
 
 
 def traceback_filepath(path: Namespace = None) -> str:
     """Construct filepath for writing traceback."""
-    path = path or default_path
+    path = path or context.default_path
     time = datetime.now().strftime('%Y%m%d-%H%M%S')
     return os.path.join(path.log, f'exception-{time}.log')
 
 
-def write_traceback(exc: Exception, site: Namespace = None, logger: logging.Logger = None,
+def write_traceback(exc: Exception, site: Namespace = None, logger: Logger = None,
                     status: int = exit_status.uncaught_exception, module: str = None) -> int:
     """Write exception to file and return exit code."""
     write = functools.partial(display_critical, module=module) if not logger else logger.critical
@@ -50,3 +58,14 @@ def write_traceback(exc: Exception, site: Namespace = None, logger: logging.Logg
     write(f'{exc.__class__.__name__}: ' + str(exc).replace('\n', ' - '))
     write(f'Exception traceback written to {path}')
     return status
+
+
+try:
+    context, config = Configuration.from_context('plot', create_dirs=True,
+                                                 config_format='toml', default_config=default_config)
+    Logger.default(__name__,
+                   format=config.logging.format,
+                   level=level_by_name.get(config.logging.level.upper()))
+except Exception as err:
+    write_traceback(err, module=__name__)
+    sys.exit(exit_status.bad_config)
--- src/plot_cli/core/__init__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2022 Geoffrey Lentner
-# SPDX-License-Identifier: Apache-2.0
-
-"""Core modules."""
